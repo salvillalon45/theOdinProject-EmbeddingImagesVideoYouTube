afe7e83cfb079d36aff82382bc859841a0171b0e
Salvador Villalon <salvadorvillalon54@gmail.com>
2022-01-04 13:31:21 -0800

    project complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,9 +19,8 @@ module.exports = {
 				black: '#181818',
 				lightBlack: '#202020',
 				white: '#ffffff',
-				grey: '#8f8f8f',
 				lightGrey: '#313131',
-				subText: '#827876',
+				grey: '#827876',
 				blue: '#248eff'
 			},
 			fontFamily: {
